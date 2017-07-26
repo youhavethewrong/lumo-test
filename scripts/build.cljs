f6de2b127104febc8d787a74ce8f3f19b653f540
ESC <youhavethewrong@gmail.com>
2017-07-26 12:01:35 -0500

    Cleanup.

--- scripts/build.cljs
+++ scripts/build.cljs
@@ -3,13 +3,6 @@
 (b/build "src"
          {:optimizations :advanced
           :main 'example.core
-          :npm-deps {:left-pad "1.1.3"} ;; NEW
+          :npm-deps {:left-pad "1.1.3"}
           :output-to "main.js"
           :target :nodejs})
-
-
-;; (b/build "src"
-;;   {:main 'example.core
-;;    :output-to "main.js"
-;;    :optimizations :advanced
-;;    :target :nodejs})
